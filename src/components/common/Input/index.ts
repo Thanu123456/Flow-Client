87256141fed44f86b9b4cc45dfec63629e4dd236
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2025-10-17 22:07:38 +0530

    PP004 - Common Input  element implementation added

---
+++ src/components/common/Input/index.ts
@@ -0,0 +1,16 @@
+export {
+  TextInput,
+  NumberInput,
+  PasswordInput,
+  SearchInput,
+  PINInput,
+  TextAreaInput,
+  EmailInput,
+  PhoneInput,
+  CurrencyInput,
+  PercentageInput,
+  BarcodeInput,
+  UserIDInput,
+} from "./Input";
+
+export * from "./Input.types";
