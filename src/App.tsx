0b91b9f64d1b3622694588af4e4b0e94207b84fe
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2025-10-19 19:50:04 +0530

    PP006 - Common Sidebar, Navigation implementation is done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { ConfigProvider } from "antd";
 import { customTheme } from "./config/theme.config";
-import { ButtonExamples } from "./components/common/Button/ButtonExamples";
-import InputExamples from "./components/common/Input/InputExample";
-import { TableExample } from "./components/common/Table/TableExample";
+// import { ButtonExamples } from "./components/common/Button/ButtonExamples";
+// import InputExamples from "./components/common/Input/InputExample";
+// import { TableExample } from "./components/common/Table/TableExample";
+import { NavigationExample } from "./components/common/Navigation/NavigationExample";
 
 const App = () => (
   <ConfigProvider theme={customTheme}>
     {/* <ButtonExamples /> */}
     {/* <InputExamples /> */}
-    <TableExample />
+    {/* <TableExample /> */}
+    <NavigationExample />
   </ConfigProvider>
 );
 
